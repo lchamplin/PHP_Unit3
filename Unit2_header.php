e3191f14be7057d800f1a8fcba74fb5e2e5397bb
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-13 20:25:41 -0700

    initial commit

---
+++ Unit2_header.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<head>
+	<title>PHP Header</title>
+	<meta charset="UTF-8">
+	<meta name="author" content="Lauren Champlin">
+	<link rel="stylesheet" href="Unit2_common.css">
+	
+</head>
+<header id="header"> 
+<h1>Candy Shop</h1>
+</header>
+<nav>
+        <div class="topnav">
+                <a href="Unit2_store.php">Candy</a>
+                <a href="Unit2_store.php">Cupcakes</a>
+                <a href="Unit2_store.php">Ice Cream</a>
+                <a id="admin" href="Unit2_admin.php">Admin</a>
+        </div>
+</nav>
