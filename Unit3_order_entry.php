e14ad319796c4933a6513570d0822c4f3bbd9d4f
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-13 22:42:06 -0700

    fixed function

--- Unit3_order_entry.php
+++ Unit3_order_entry.php
@@ -72,8 +72,8 @@
                 })
         })
         function showStock(str) {
-                console.log("show stock");
                 if(str){
+                        console.log("show stock");
                         const xhttp = new XMLHttpRequest();
                         xhttp.onreadystatechange = function() {
                                 if (this.readyState == 4 && this.status == 200){
