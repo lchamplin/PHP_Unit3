7ad358fe7e762322dfd5ad92011441a3fe64100a
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-19 19:25:18 -0700

    part3

--- Unit3_ajaxsubmit.php
+++ Unit3_ajaxsubmit.php
@@ -5,7 +5,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', True);
 ?>
 <?php
-$conn = getConnection();
+include "Unit3_database_credentials.php";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
 
 
 function processOrder(){
